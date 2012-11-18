c7013ccf88088fcfab063f195d424a9cf08f31b3
jessada <jessada@kth>
2012-11-19 00:28:41 +0100

    updated on Nov 19

--- runpdf.sh
+++ runpdf.sh
@@ -1,22 +1,11 @@
 latex sim_pae_diary
-bibtex sim_pae_diary
-latex sim_pae_diary
-latex sim_pae_diary
 dvipdfm sim_pae_diary
 if [ -e sim_pae_diary.pdf ]
 then
     rm *.aux
-    rm *.auxlock
-    rm *.bbl
-    rm *.blg
-    rm *.dpth
     rm *.dvi
-    rm *.glo
-    rm *.idx
-    rm *.ist
     rm *.log
     rm *.out
-    rm *.spl
 
 
     evince sim_pae_diary.pdf &
